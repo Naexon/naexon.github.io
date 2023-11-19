876a3752a04fa19535fe13271bafdcd569fe877a
Naexon <naexoncode@gmail.com>
2023-11-19 23:43:20 +0100

    fixed script link

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,7 @@
-export const main = () => {
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-    window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-};
+});
