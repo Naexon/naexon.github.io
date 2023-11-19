b7e34df19747fddc1b321e02843bf293309b5f6b
Naexon <naexoncode@gmail.com>
2023-11-19 23:39:28 +0100

    added export to ts script

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,9 @@
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
+export const main = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+};
