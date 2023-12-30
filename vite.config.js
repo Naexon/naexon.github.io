1d2d398c5bc852f6120a064fe8b51cf090286335
Naexon <naexoncode@gmail.com>
2023-12-30 20:41:00 +0100

    initial prototype for 404 page

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({_command, _mode}) => ({
     base: "/",
-    // base: command === "build" ? "/website/" : "/",
+    root: "src",
+    build: {
+        outDir: "../dist",
+    },
 }));
